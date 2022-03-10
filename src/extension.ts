e8cf911b2b846e09af9b0c5e32cfbe13c1e8ddd7
MBearo <wuweiqi@baidu.com>
2022-03-10 15:24:10 +0800

    暂存

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,31 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "x-tools" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('x-tools.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from x-tools!');
-	});
-
-	context.subscriptions.push(disposable);
+	var thisProvider = {
+		resolveWebviewView: function (webviewView: vscode.WebviewView) {
+			webviewView.webview.options = { enableScripts: true };
+			webviewView.webview.html = `
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Document</title>
+			</head>
+			<body>
+				hahahaha
+				<button>aaaa</button
+			</body>
+			</html>
+			`;
+		}
+	};
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider("xTools.Webview", thisProvider)
+	);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
